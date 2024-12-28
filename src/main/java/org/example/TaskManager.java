b7c61d2a577c0398866d648a65ffb4b9ac8df761
ZvedamRance <Dolisko@seznam.cz>
2024-12-28 14:47:19 +0100

    Oprava TaskManager

--- src/main/java/org/example/TaskManager.java
+++ src/main/java/org/example/TaskManager.java
@@ -8,89 +8,78 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class TaskManager {
-    static File file = new File("tasks.csv");
-    static String[][] tasks;
-    static final String[] OPTIONS = {"add", "remove", "list", "exit"};
-    public static void showMainMenu() {
-        System.out.println(ConsoleColors.BLUE + "\nPlease select an option: " + ConsoleColors.RESET);
-        for (String option : OPTIONS) {
-            System.out.println("-> " + option);
-        }
-        menuButtons();
-    }
 
-    public static void menuButtons() {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+class TaskManager {
+    private static final File file = new File("tasks.csv");
+    private static String[][] tasks;
+    private static final String[] OPTIONS = {"add", "remove", "list", "exit"};
+    private static Scanner scanner = new Scanner(System.in);
 
-        switch (input) {
-            case "add":
-                add();
-                break;
-            case "remove":
-                remove();
-                break;
-            case "list":
-                list();
-                break;
-            case "exit":
-                exit();
-                break;
-            default:
-                System.out.println(ConsoleColors.RED + "Please select a correct option." + ConsoleColors.RESET);
-                menuButtons();
-                break;
+    public static void main(String[] args) {
+        loadTasks();
+
+        while (true) {
+            System.out.println(ConsoleColors.BLUE + "\nPlease select an option: " + ConsoleColors.RESET);
+            for (String option : OPTIONS) {
+                System.out.println("-> " + option);
+            }
+
+            final String input = scanner.nextLine();
+            switch (input) {
+                case "add" -> add();
+                case "remove" -> remove();
+                case "list" -> list();
+                case "exit" -> exit();
+                default -> System.out.println(ConsoleColors.RED + "Please select a correct option." + ConsoleColors.RESET);
+            }
         }
     }
 
     public static void add() {
-        Scanner scanner = new Scanner(System.in);
         tasks = Arrays.copyOf(tasks, tasks.length + 1);
         System.out.println("Please enter the name of the task you would like to add:");
-        String name = scanner.nextLine();
+        final String name = scanner.nextLine();
         System.out.println("Please enter the date (format YYYY-MM-DD):");
-        String date = scanner.nextLine();
+        final String date = scanner.nextLine();
         System.out.println("Please enter if the task is done or not (true/false):");
-        String bool = scanner.nextLine();
-        tasks[tasks.length - 1] = new String[] {name, date, bool};
-        showMainMenu();
+        final String isDone = scanner.nextLine();
+        tasks[tasks.length - 1] = new String[] {name, date, isDone};
     }
 
     public static void remove() {
         System.out.println("Please, enter the number of the task you would like to remove:");
         int number = -1;
-        Scanner scanner = new Scanner(System.in);
         while (true) {
-            String input = scanner.nextLine();
+            final String input = scanner.nextLine();
             try{
                 number = Integer.parseInt(input);
             } catch (NumberFormatException e) {
             }
-            if (number >= 0 && number < tasks.length-1) {
+            if (number >= 0 && number < tasks.length) {
                 break;
             } else {
                 System.out.println(ConsoleColors.RED + "Please, enter a number between 0 and " + (tasks.length - 1) + "." + ConsoleColors.RESET);
             }
         }
         tasks = ArrayUtils.remove(tasks, number);
-        showMainMenu();
     }
 
-    public static void list() {
-        for (int i = 0; i < tasks.length; i++) {
-            for (int j =0 ; j < 3; j++) {
-                if (j < 2) {
-                    System.out.print(ConsoleColors.RESET + tasks[i][j]+ ", ");
-                } else {
-                    System.out.print(ConsoleColors.RESET + tasks[i][j]);
-                }
-            }
-            System.out.println();
+
+    private static void list() {
+        System.out.println(ConsoleColors.PURPLE + "Id:[Note, Date, IsDone]");
+        System.out.println(ConsoleColors.RESET + "-----------------");
+        for (int i= 0; i < tasks.length; i++) {
+            System.out.println( i + ":"  + Arrays.toString(tasks[i]));
         }
-        showMainMenu();
     }
+
     public static void exit() {
+        saveTasks();
+        System.out.println(ConsoleColors.RED + "\nProgram terminated.");
+        scanner.close();
+        System.exit(0);
+    }
+    public static void saveTasks () {
         try (Writer writer = new FileWriter(file)) {
             for (int i = 0; i < tasks.length; i++) {
                 for (int j =0 ; j < 3; j++) {
@@ -102,32 +91,29 @@ public class TaskManager {
                 }
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Failed to save tasks into file.");
+            e.printStackTrace(System.err);
+            System.exit(1);
         }
-        System.out.println(ConsoleColors.RED + "\nProgram terminated.");
-        System.exit(0);
     }
-    public static void retrievingData () {
-        ArrayList<String> list = new ArrayList<>();
+    public static void loadTasks () {
+        final ArrayList<String> lines = new ArrayList<>();
 
         try (Scanner scanner = new Scanner(file)) {
             while (scanner.hasNextLine()) {
-                list.add(scanner.nextLine());
+                lines.add(scanner.nextLine());
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Failed to load tasks.csv");
+            e.printStackTrace(System.err);
+            System.exit(1);
         }
-        tasks = new String[list.size()][3];
-        for (int i = 0; i < list.size(); i++) {
+        tasks = new String[lines.size()][3];
+        for (int i = 0; i < lines.size(); i++) {
             for (int j = 0; j < 3; j++) {
-                String [] temp = list.get(i).split(",");
+                String [] temp = lines.get(i).split(",");
                 tasks[i][j] = temp [j].trim();
             }
         }
-    }
-
-    public static void main(String[] args) {
-        retrievingData();
-        showMainMenu();
     }
 }
